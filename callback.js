de3606a161020d73e72346e84801d1ff4297ff6e
Abhay <146931800+abhaydubey200@users.noreply.github.com>
2024-02-12 16:27:14 +0530

    its third

--- callback.js
+++ callback.js
@@ -1,4 +1,17 @@
-const add=(a,b)=>{
-    return a+b;
-}
-console.log(add(3,5));
+
+function add(a, b, callback) {
+    console.log("Adding", a, "and", b);
+    const sum = a + b;
+    console.log("Sum calculated:", sum);
+   
+    callback(sum);
+  }
+  function displayResult(result) {
+    console.log("Received result:", result);
+    console.log("The result is:", result);
+  }
+  
+  console.log("Calling add function...");
+  
+  add(5, 3, displayResult);
+  
