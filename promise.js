75de4b4c6dc3a1c3876f1be529641d8def5b212c
Abhay <146931800+abhaydubey200@users.noreply.github.com>
2024-02-13 11:06:11 +0530

    this is promise

--- promise.js
+++ promise.js
@@ -1,18 +1,25 @@
-const ticket=new Promise(function(done, notdone){
- const isboarded=false;
- if(isboarded){
-    done("is it done:---");
- }
- else{
-    notdone("why is it done:---");
- }
-});
-ticket.then((data)=>{
-    console.log("wow",data);
-})
-.catch((data)=>{
-    console.log("oh no",data);
-})
-.finally((data)=>{
-    console.log("Good Night:----");
-})
+// const ticket=new Promise(function(done, notdone){
+//  const isboarded=false;
+//  if(isboarded){
+//     done("is it done:---");
+//  }
+//  else{
+//     notdone("why is it done:---");
+//  }
+// });
+// ticket.then((data)=>{
+//     console.log("wow",data);
+// })
+// .catch((data)=>{
+//     console.log("oh no",data);
+// })
+// .finally((data)=>{
+//     console.log("Good Night:----");
+// })
+
+
+
+
+
+
+
