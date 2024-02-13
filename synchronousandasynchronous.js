ebd28a91c06bb079cc0bba76dbdcd323d6ad1029
Abhay <146931800+abhaydubey200@users.noreply.github.com>
2024-02-13 10:29:56 +0530

    f

--- synchronousandasynchronous.js
+++ synchronousandasynchronous.js
@@ -1,17 +1,33 @@
 //this is synchronous function,this will exquit one by one:----
 
-function synchronousFunction() {
-    console.log('Synchronous function is executing...');
-}
+// function synchronousFunction() {
+//     console.log('Synchronous function is executing...');
+// }
 
-console.log('Before calling synchronous function');
-synchronousFunction();
-console.log('After calling synchronous function');
+// console.log('Before calling synchronous function');
+// synchronousFunction();
+// console.log('After calling synchronous function');
 
 
 
 //this is synchronous function,this will exiqute after complting ove time:----
 
+// setTimeout(function(){
+//     const name=prompt("your name");
+//     console.log(name);
+// }, 3000)
+
+
+
 setTimeout(function(){
-    console.log("Hello")
-}, 3000)
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+      
+      readline.question('What is your name?', name => {
+        console.log("hello",name);
+        
+        readline.close();
+      });
+}, 30000)
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,7 @@
-object
+function synchronousFunction() {
+//     console.log('Synchronous function is executing...');
+// }
+
+// console.log('Before calling synchronous function');
+// synchronousFunction();
+// console.log('After calling synchronous function');
