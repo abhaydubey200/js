faa8262905c04a274dfab06493e9e8114729d0f0
Abhay <146931800+abhaydubey200@users.noreply.github.com>
2024-02-13 11:13:54 +0530

    main

--- promise.js
+++ promise.js
@@ -1,3 +1,19 @@
+// A JavaScript Promise object can be:
+
+// Pending
+// Fulfilled
+// Rejected
+// The Promise object supports two properties: state and result.
+
+// While a Promise object is "pending" (working), the result is undefined.
+
+// When a Promise object is "fulfilled", the result is a value.
+
+// When a Promise object is "rejected", the result is an error object.
+
+
+
+
 // const ticket=new Promise(function(done, notdone){
 //  const isboarded=false;
 //  if(isboarded){
