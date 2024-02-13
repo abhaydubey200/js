2ebb9ecc2eb903bcb0d067c9e41cb86834f1ae44
Abhay <146931800+abhaydubey200@users.noreply.github.com>
2024-02-13 11:02:18 +0530

    s

---
+++ promise.js
@@ -0,0 +1,18 @@
+const ticket=new Promise(function(done, notdone){
+ const isboarded=false;
+ if(isboarded){
+    done("is it done:---");
+ }
+ else{
+    notdone("why is it done:---");
+ }
+});
+ticket.then((data)=>{
+    console.log("wow",data);
+})
+.catch((data)=>{
+    console.log("oh no",data);
+})
+.finally((data)=>{
+    console.log("Good Night:----");
+})
